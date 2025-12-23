30f0a95797cde4bcb27cd0de4a30aad44667f9b7
notificacionhe5 <notificacionhe5@gmail.com>
2025-12-23 17:05:40 +0000

    routes protected

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,27 +1,37 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import LoginPage from '../pages/LoginPage';
 import Principal from '../pages/Principal';
 import GestionTablas from '../pages/GestionTablas';
-import Etiquetas from '../pages/Etiquetas'; 
+import Etiquetas from '../pages/Etiquetas';
 import TaskDetail from '../pages/TaskDetail';
 import UserProfile from '../pages/UserProfile';
 import UserManagement from '../pages/UserManagement';
 
+// Importamos el guardián que creamos
+import ProtectedRoute from './ProtectedRoute';
 
 export default function AppRouter() {
-return(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/principal" element={<Principal />} />
-      <Route path="/gestion-tablas" element={<GestionTablas />} />
-      <Route path="/etiquetas" element={<Etiquetas />} />
-      <Route path="/task-detail/:id" element={<TaskDetail />} />
-      <Route path="/user-profile" element={<UserProfile />} />
-      <Route path="/user-management" element={<UserManagement />} />
-    </Routes>
-  </BrowserRouter>
-);
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Ruta pública: cualquiera puede acceder a /login */}
+        <Route path="/login" element={<LoginPage />} />
 
+        {/* Rutas Protegidas: solo usuarios con token válido pueden acceder */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/principal" element={<Principal />} />
+          <Route path="/gestion-tablas" element={<GestionTablas />} />
+          <Route path="/etiquetas" element={<Etiquetas />} />
+          <Route path="/task-detail/:id" element={<TaskDetail />} />
+          <Route path="/user-profile" element={<UserProfile />} />
+          <Route path="/user-management" element={<UserManagement />} />
+        </Route>
+
+        {/* (Opcional) Una ruta por defecto para redirigir si se entra a la raíz "/" */}
+        <Route path="/" element={<Navigate to="/principal" replace />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
